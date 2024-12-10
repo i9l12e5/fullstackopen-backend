3604bea24372968a7e92751fe86bda7e21580722
Ilkka Häyrinen <iph.viva@gmail.com>
2024-12-10 02:25:18 +0200

    Customer schema formatted

--- models/customer.js
+++ models/customer.js
@@ -5,43 +5,43 @@ mongoose.set("strictQuery", false);
 const url = process.env.MONGODB_URI;
 
 mongoose
-	.connect(url)
-	.then((result) => {
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message);
-	});
+    .connect(url)
+    .then((result) => {
+        console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message);
+    });
 
 const contactSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		minLength: 3,
-		required: true,
-	},
-	number: {
-		type: String,
-		minLength: 8,
-		required: true,
-		validate: {
-			validator: function (v) {
-				return /^\d{2,3}-\S{5,}$/.test(v);
-			},
-			message: (props) => `${props.value} is not a valid phone number!`,
-		},
-	},
-	id: {
-		type: Number,
-		required: true,
-		unique: true,
-	},
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\S{5,}$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+    },
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
 });
 
 contactSchema.set("toJSON", {
-	transform: (document, returnedObject) => {
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+    transform: (document, returnedObject) => {
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
 module.exports = mongoose.model("Contact", contactSchema);
