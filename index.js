72def4563d76a803026ff705f6dd2239d1508af2
Ilkka Häyrinen <iph.viva@gmail.com>
2024-12-10 02:14:48 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const cors = require("cors");
+const contactSchema = require("./models/customer");
 
 const PORT = process.env.PORT || 3001;
 
-let persons = [
-	{
-		name: "Arto Hellas",
-		number: "040-123456",
-		id: "1",
-	},
-	{
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-		id: "2",
-	},
-	{
-		name: "Dan Abramov",
-		number: "12-43-234345",
-		id: "3",
-	},
-	{
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-		id: "4",
-	},
-];
+const Contact = contactSchema;
 
-const generateId = () => {
-	// added this bit to prevent any chances of duplicate ids
-	const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-	return Number(maxId + 1).toString(); // return number as string to match hardcoded value type
+const countPersons = async () => {
+	try {
+		const count = await contactSchema.countDocuments({});
+
+		return count;
+	} catch (error) {
+		errorHandler(error);
+	}
 };
 
-app.use(cors());
-app.use(express.static("dist"));
+const generateId = async () => {
+	const count = await countPersons();
 
-morgan.token("body", (req) => JSON.stringify(req.body) || "");
+	if (count > 0) {
+		const result = await contactSchema.find({});
+		const ids = result.map((value) => (isFinite(value.id) ? value.id : 0)); // Fixes entries with missing id. Since there is no failsafe to guarantee broken contacts have valid unique id, we identify them with zero ID
 
+		const maxId = Math.max(...ids);
+
+		return maxId + 1;
+	} else {
+		// Default value for first one
+		return 1;
+	}
+};
+
+app.use(express.json()); // Load before morgan
+morgan.token("body", (req) => {
+	return JSON.stringify(req.body);
+}); // Fixes morgan() crash for :body
 app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-	res.send("hello");
+app.get("/", (req, res, next) => {
+	try {
+		res.send("hello");
+	} catch (error) {
+		next(error);
+	}
 });
 
-app.get("/api/persons", (req, res) => {
-	res.json(persons);
+// Route to query all contacts
+app.get("/api/persons", (req, res, next) => {
+	contactSchema
+		.find({})
+		.then((result) => res.json(result))
+		.catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
-	console.log("....", req.body);
+// Route to add new contact
+app.post("/api/persons", async (req, res, next) => {
 	const newName = req.body.name;
 	const newNumber = req.body.number;
 
@@ -64,42 +68,107 @@ app.post("/api/persons", (req, res) => {
 			.json({ error: `${!newName ? "Name" : "Number"} is missing` })
 			.end();
 
-	const checkForDuplicates = persons.some((user) => user.name === newName);
+	const getId = await generateId();
 
-	if (checkForDuplicates)
-		return res.status(400).json({ error: "Name must be unique" }).end();
-
-	const newPerson = {
+	const newContact = new Contact({
 		name: newName,
 		number: newNumber,
-		id: generateId(),
-	};
+		id: getId,
+	});
 
-	persons.push(newPerson);
+	const newEntry = newContact;
 
-	return res.status(201).end();
+	contactSchema
+		.findOne({ name: newName })
+		.then((result) => {
+			if (result !== null)
+				return res.status(400).json({ error: "Name must be unique" }).end();
+			else {
+				newEntry
+					.save()
+					.then(() => {
+						return res.status(201).end();
+					})
+					.catch((error) => next(error));
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+// Route to query single contact by id
+app.get("/api/persons/:id", (req, res, next) => {
+	const id = Number(req.params.id);
+
+	contactSchema
+		.findOne({ id: id })
+		.then((result) => {
+			if (!result) {
+				const error = "User not found";
+				next(error);
+			} else res.send(`User ${result.name} phone number is ${result.number}`);
+		})
+		.catch((error) => next(error));
+});
+
+// Route to update contact number
+app.put("/api/persons/:id", (req, res, next) => {
 	const id = req.params.id;
-	const find = persons.find((person) => person.id === id);
+	const body = req.body;
 
-	if (find === undefined) res.send("404 - user not found");
-	else res.send(`User ${find.name} phone number is ${find.number}`);
+	// TODO: korjaa
+	contactSchema
+		.findOneAndUpdate(
+			{ id: id },
+			{ $set: { number: body.number } },
+			{ returnNewDocument: true },
+		)
+		.then((result) => {
+			console.log(result);
+
+			if (result) res.json(result);
+			else res.status(404);
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+// Route to delete contact by id
+app.delete("/api/persons/:id", (req, res, next) => {
 	const id = req.params.id;
 
-	persons = persons.filter((person) => person.id !== id);
-
-	res.status(204).end();
+	contactSchema
+		.findOneAndDelete({ id: id })
+		.then((result) => {
+			res.json(result).end();
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-	const text = `Phonebook has info for ${persons.length} people <br /><br />${new Date().toUTCString()}`;
-	res.send(text);
+// Returns HTML
+app.get("/info", async (req, res, next) => {
+	try {
+		const persons = await countPersons();
+
+		const text = `Phonebook has info for ${persons} people <br /><br />${new Date().toUTCString()}`;
+
+		res.send(text);
+	} catch (error) {
+		next(error);
+	}
 });
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT);
 
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+	console.log("give password as argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const fetchAll = process.argv.length === 3 ? true : false;
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://rantest:${password}@cluster0.ejrwb.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const contactSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+const newContact = new Contact({
+	name: name,
+	number: number,
+});
+
+if (fetchAll) {
+	Contact.find({}).then((persons) => {
+		console.log("Phonebook:");
+		persons.forEach((person) => {
+			console.log(`${person.name} ${person.number}`);
+		});
+		mongoose.connection.close();
+	});
+} else {
+	newContact.save().then((result) => {
+		console.log(`Added ${name} number ${number} to phonebook`);
+		mongoose.connection.close();
+	});
+}
